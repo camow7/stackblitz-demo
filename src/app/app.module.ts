dbbf18a3e9d976a9bc743295d26ae75d31daf804
camow7 <cameron.owen@live.com.au>
2019-10-17 22:20:18 +1100

    Added routes, home page and profile page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, MDBBootstrapModule.forRoot() ],
-  declarations: [ AppComponent, HelloComponent, HomeComponent ],
+  imports:      [ BrowserModule, FormsModule, MDBBootstrapModule.forRoot(), RouterModule.forRoot([
+    {path: '', component: HomeComponent},
+    {path: 'profile', component: ProfileComponent}
+  ]) ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, ProfileComponent, NavbarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
